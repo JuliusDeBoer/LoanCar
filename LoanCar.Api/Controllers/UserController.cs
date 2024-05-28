e6f89c921d3d6cfb21fbff3c89aeb1379daf2e9a
Julius de Boer <45075461+JuliusDeBoer@users.noreply.github.com>
2024-05-29 01:00:36 +0200

    feat: Add basic (and insecure) user controller

--- LoanCar.Api/Controllers/UserController.cs
+++ LoanCar.Api/Controllers/UserController.cs
@@ -1,6 +1,15 @@
 using LoanCar.Api.DTOs;
 using LoanCar.Api.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// Prevent this code from going into production with insecure endpoints.
+// Remove if you dare!
+#if DEBUG
+#warning Very insecure endpoints
+#else
+#error Very insecure endpoints
+#endif
 
 namespace LoanCar.Api.Controllers
 {
@@ -8,24 +17,99 @@ namespace LoanCar.Api.Controllers
     [Route("users")]
     public class UserController(LoanCarContext db) : ControllerBase
     {
-        private readonly LoanCarContext _db = db;
+        private readonly LoanCarContext db = db;
 
         [HttpGet]
         public IActionResult Get()
         {
-            throw new NotImplementedException();
+            var users = db.Users.Select(u => new PublicUserDTO()
+            {
+                Id = u.Id,
+                Email = u.Email,
+                FirstName = u.FirstName,
+                LastName = u.LastName,
+                IsAdmin = u.IsAdmin
+            }).ToList();
+
+            return Ok(users);
         }
 
-        [HttpGet("{_id}")]
-        public IActionResult GetById(int _id)
+        // TODO: Write a DTO for this
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid id)
         {
-            throw new NotImplementedException();
+            var user = db.Users.FirstOrDefault(u => u.Id == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(new PublicUserDTO()
+            {
+                Id = user.Id,
+                Email = user.Email,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                IsAdmin = user.IsAdmin
+            });
         }
 
         [HttpPost]
-        public IActionResult Post([FromBody] NewUserDTO _userDTO)
+        public IActionResult Post([FromBody] NewUserDTO dto)
         {
-            throw new NotImplementedException();
+            var password = BCrypt.Net.BCrypt.HashPassword(dto.Password);
+
+            var user = new User()
+            {
+                Email = dto.Email,
+                FirstName = dto.FirstName,
+                LastName = dto.LastName,
+                IsAdmin = dto.IsAdmin,
+                Password = password
+            };
+
+            db.Users.Add(user);
+
+            db.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(Guid id, [FromBody] UpdateUserDTO dto)
+        {
+            var user = db.Users.FirstOrDefault(u => u.Id == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            user.Email = dto.Email;
+            user.FirstName = dto.FirstName;
+            user.LastName = dto.LastName;
+            user.IsAdmin = dto.IsAdmin;
+
+            db.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var user = db.Users.Where(u => u.Id == id);
+
+            if (!user.Any())
+            {
+                return NotFound();
+            }
+
+            user.ExecuteDelete();
+
+            db.SaveChanges();
+            return Ok();
         }
     }
 }
--- LoanCar.Api/DTOs/PublicUserDTO.cs
+++ LoanCar.Api/DTOs/PublicUserDTO.cs
@@ -1,11 +1,11 @@
 ﻿namespace LoanCar.Api.DTOs
 {
-    public class NewUserDTO
+    public class PublicUserDTO
     {
+        public Guid Id { get; set; }
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
         public bool IsAdmin { get; set; }
     }
 }
