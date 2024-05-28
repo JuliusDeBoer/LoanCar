86530582ba7eab3e8151c338c3556e28892a7a20
Julius de Boer <45075461+JuliusDeBoer@users.noreply.github.com>
2024-05-29 01:45:46 +0200

    feat: Add basic notification controller

---
+++ LoanCar.Api/DTOs/PublicNotificationDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace LoanCar.Api.DTOs
+{
+    public class PublicNotificationDTO
+    {
+        public Guid Id { get; set; }
+        public string Message { get; set; } = string.Empty;
+    }
+}
