acb0546d9f8d5cce0a5f90ec5d11d8d5195de1e6
Julius de Boer <45075461+JuliusDeBoer@users.noreply.github.com>
2024-05-29 02:57:31 +0200

    feat: Add basic report controller

---
+++ LoanCar.Api/DTOs/NewReportDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace LoanCar.Api.DTOs
+{
+    public class NewReportDTO
+    {
+        public Guid ReservationId { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty; // Base64 encoded
+    }
+}
